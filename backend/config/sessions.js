11aa2d7d4f6088fe5b20086559f59e53e9b235d7
andrianwahyu1310 <andrian.wahyu1310@gmail.com>
2026-07-26 12:31:38 +0700

    Debugging

--- backend/config/sessions.js
+++ backend/config/sessions.js
@@ -4,6 +4,7 @@ const sessionConfig = (options = {}) => session({
     secret: options.secret || process.env.TOKEN_SECRET || "fallback-secret",
     resave: false,
     saveUninitialized: false,
+    proxy: true,
     cookie: {
         secure: process.env.NODE_ENV === "production",
         sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
